ba240c54edef11fdcf020a9bb2936e89dfae2ee1
Rushion <rushionchegge@gmail.com>
2024-07-22 10:56:28 +0300

    manipulated demo.js and rewrote index.js

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-/* This is a file inserted to keep the learn IDE browser happy */
-const testVar = {}
+// This is a file inserted to keep the learn IDE browser happy
+const testVar = {};
+
+// Export testVar for testing purposes
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = { testVar };
+}
